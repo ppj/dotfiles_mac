04ada43e2bcf6ac9c1595d993346a051d930a4ba
Prasanna Joshi <prasanna_3003@yahoo.com>
2017-06-21 14:56:42 +1000

    Emacs config - initial commit

---
+++ init.el
@@ -0,0 +1,55 @@
+;; Auto added stuff
+;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (wombat)))
+ '(package-selected-packages (quote (org-bullets which-key try use-package))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; My settings below
+;;
+(setq inhibit-startup-message t)
+
+;; Look & Feel
+;;
+(set-default-font "Monaco 16")
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package try
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+(use-package org-bullets
+  :ensure t
+  :config (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(use-package evil
+  :ensure t
+  :config
+    (evil-mode 1)
+    (setq evil-overriding-maps nil)
+    (setq evil-intercept-maps nil)
+)
