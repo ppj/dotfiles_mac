4c21cb2927b3cb96a525ec72b847e4e84a64c046
Prasanna Joshi <prasanna_3003@yahoo.com>
2017-06-27 00:03:39 +1000

    Change mode-line colors based on state

--- init.el
+++ init.el
@@ -122,3 +122,16 @@
 
 (linum-mode t) ;; line numbers on
 
+;; change mode-line color by evil state
+(eval-when-compile (require 'cl))
+(lexical-let ((default-color (cons (face-background 'mode-line)
+                                   (face-foreground 'mode-line))))
+             (add-hook 'post-command-hook
+                       (lambda ()
+                         (let ((color (cond ((minibufferp) default-color)
+                                            ((evil-insert-state-p) '("#444488" . "#ffffff"))
+                                            ((evil-emacs-state-p)  '("#e80000" . "#ffffff"))
+                                            ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
+                                            (t default-color))))
+                           (set-face-background 'mode-line (car color))
+                           (set-face-foreground 'mode-line (cdr color))))))
