39fc5917d52a17b464ba95b7201736546bcf4cad
Prasanna Joshi <prasanna_3003@yahoo.com>
2017-06-27 11:24:53 +1000

    The separated custom.el to store custom-set vars

---
+++ custom.el
@@ -0,0 +1,21 @@
+;; Auto added
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (seti)))
+ '(custom-safe-themes
+   (quote
+    ("a632c5ce9bd5bcdbb7e22bf278d802711074413fd5f681f39f21d340064ff292" default)))
+ '(package-selected-packages
+   (quote
+    (evil-surround evil-leader auto-complete org-bullets which-key try use-package))))
+
+;; Auto added
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
