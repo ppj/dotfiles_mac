a1d8dd9954643bc395f962b0c84e9b0baddabebe
Prasanna Joshi <prasanna_3003@yahoo.com>
2017-07-06 02:11:21 +1000

    Remove the coloring of mode-line based on states & buffer save status

--- init.el
+++ init.el
@@ -197,20 +197,6 @@
   (setq nlinum-relative-redisplay-delay 0)
   (add-hook 'prog-mode-hook #'nlinum-relative-mode))
 
-;; change mode-line color by evil state
-(eval-when-compile (require 'cl))
-(lexical-let ((default-color (cons (face-background 'mode-line)
-                                   (face-foreground 'mode-line))))
-             (add-hook 'post-command-hook
-                       (lambda ()
-                         (let ((color (cond ((minibufferp) default-color)
-                                            ((evil-insert-state-p) '("#444488" . "#ffffff"))
-                                            ((evil-emacs-state-p)  '("#e80000" . "#ffffff"))
-                                            ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
-                                            (t default-color))))
-                           (set-face-background 'mode-line (car color))
-                           (set-face-foreground 'mode-line (cdr color))))))
-
 ;; display full file path in the frame title
 (setq frame-title-format
       '(:eval
