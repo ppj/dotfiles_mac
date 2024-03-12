eaa1345a8c1f012da0148e8ec10d81730688de68
Prasanna Joshi <prasanna.joshi@cultureamp.com>
2024-03-12 22:38:38 +1100

    Overwrite [c with [g and ]c with ]g for vimdiff and gitsigns consistent diff motions

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -7,8 +7,18 @@ return {
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
-
   config = function()
+    require("neo-tree").setup {
+      window = {
+        mappings = { -- diff navigation consistent with gitsigns & vimdiff
+          ["[c"] = "prev_git_modified",
+          ["[g"] = "nop",
+          ["]c"] = "next_git_modified",
+          ["]g"] = "nop",
+        },
+      },
+    }
+
     -- mappings
     vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left toggle<CR>", { desc = "[N]eoTree Toggle" })
   end,
