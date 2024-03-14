c905d8f0d5c6c79e6946398d87afbcdca03a936a
Prasanna Joshi <prasanna.joshi@cultureamp.com>
2024-03-15 02:07:30 +1100

    Prevent formatting files in the git index (to be staged)

--- nvim/lua/plugins/format_and_lint.lua
+++ nvim/lua/plugins/format_and_lint.lua
@@ -2,10 +2,14 @@ return { -- Autoformat
   "stevearc/conform.nvim",
   opts = {
     notify_on_error = true,
-    format_on_save = {
-      timeout_ms = 2000,
-      lsp_fallback = true,
-    },
+    format_on_save = function(bufnum)
+      -- Disable autoformat for files being staged in git
+      local bufname = vim.api.nvim_buf_get_name(bufnum)
+      if bufname:match "/.git//0/" then
+        return
+      end
+      return { timeout_ms = 2000, lsp_fallback = true }
+    end,
     formatters_by_ft = {
       lua = { "stylua" },
       -- Conform can also run multiple formatters sequentially
