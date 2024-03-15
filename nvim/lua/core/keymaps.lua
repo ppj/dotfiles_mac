ad57d896855d4d4977b42ac925560ad5af6bfa71
Prasanna Joshi <prasanna.joshi@cultureamp.com>
2024-03-16 01:40:53 +1100

    Disable mini.ai as it's interfering with highlight on yank autocmd Highlight on yank for a bit longer

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -67,7 +67,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank { on_visual = false, timeout = 250 }
+    vim.highlight.on_yank { on_visual = false, timeout = 400 }
   end,
 })
 
